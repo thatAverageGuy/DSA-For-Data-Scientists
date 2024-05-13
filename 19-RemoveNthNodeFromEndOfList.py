71a521c17f86b15d12e7ec44f287f3727e31dcad
Yogesh Singh <yogesh.singh@venaai.com>
2024-05-13 12:30:18 +0530

    Day 11

---
+++ 19-RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,52 @@
+# 19. Remove Nth Node From End of List
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+# Example 2:
+
+# Input: head = [1], n = 1
+# Output: []
+# Example 3:
+
+# Input: head = [1,2], n = 1
+# Output: [1]
+ 
+
+# Constraints:
+
+# The number of nodes in the list is sz.
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+ 
+# Follow up: Could you do this in one pass?
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head, n):
+    lenc = head
+    count = 0
+    while lenc:
+        count+=1
+        lenc = lenc.next
+
+    nodenum = count-n
+    curr = head
+    prev = head
+    if nodenum==0:
+        return prev.next
+    
+    while nodenum:
+        curr = head
+        head = head.next
+        nodenum-=1
+    curr.next = head.next
+
+    return prev
